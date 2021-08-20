b0688e0faf19d612ff7a2112f6e11b3577448055
ChenghanY <chenghan_yang@163.com>
2021-08-20 18:30:46 +0800

    测试

--- CLion/CourseBook/0201_SqList/SqList.h
+++ CLion/CourseBook/0201_SqList/SqList.h
@@ -26,7 +26,7 @@ typedef int ElemType;
 typedef struct {
     ElemType* elem;     // 顺序表存储空间的基址（指向顺序表所占内存的起始位置）
     int length;   // 当前顺序表长度（包含多少元素）
-    int listsize; // 当前分配的存储容量（可以存储多少元素）
+    int listsize; // 当前分配的存储容量（可以存储多少元素  111
 } SqList;
 
 
